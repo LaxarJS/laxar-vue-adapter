7caa18f90bd2290e4ec8956a265eb75b9e20465d
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-11-08 17:56:01 +0100

    (#1) Use changes from LaxarJS/laxar#390, add more examples

--- README.md
+++ README.md
@@ -70,3 +70,81 @@ module.exports = {
    }
 };
 ```
+
+# Injections
+
+You can inject _LaxarJS_ [widget services](https://github.com/LaxarJS/laxar/blob/master/docs/manuals/widget_services.md)
+into your Vue.js component by using the `injections` option. Your component will then be instantiated with
+an object containing the requested services. The injections will also be available as `this.$injections` in your
+component.
+
+```js
+export default {
+   injections: [ 'axEventBus', 'axGlobalLog' ],
+   created() {
+      const { axEventBus, axGlobalLog } = this.$options.injections;
+      axEventBus === this.$injections[ 0 ];
+      axGlobalLog === this.$injections[ 1 ];
+   }
+};
+```
+
+## Default injections, axContext
+
+The [`axContext`](https://github.com/LaxarJS/laxar/blob/master/docs/manuals/widget_services.md#axcontext)
+service is automatically injected. It can be accessed via the `$data` property or as
+`this.$options.injections.axContext`. It does not appear in `this.$injections` unless you specify it in
+your component's options. This allows you to easily access (for example) the `id()` generator and
+the configured features.
+
+```js
+export default {
+   template: '<b>{{features.mytext}}</b>',
+   created() {
+      this.$data.eventBus === this.$options.injections.axContext.eventBus;
+   }
+};
+```
+
+# Controls
+
+You can specify controls in your `widget.json`. Just list a module that can resolved by the module loader.
+The control will then be registered as a component with the name specified in its `control.json`.
+
+```json
+{
+   "name": "my-widget",
+   "integration": {
+      "technology": "vue",
+      "type": "widget"
+   },
+   "controls": "my-vue-control-module"
+}
+```
+
+```json
+{
+   "name": "my-vue-control",
+   "integration": {
+      "technology": "vue",
+      "type": "control"
+   }
+}
+```
+
+```vue
+<template>
+<div>This is my widget</div>
+<my-vue-control>It's using a control</my-vue-control>
+</template>
+
+<script>
+export default {
+   components: {
+      // this part is automatically supplied by the runtime:
+      'my-vue-control': ConstructorCreatedByTheVueAdapter
+   }
+};
+</script>
+```
+
--- laxar-vue-adapter.js
+++ laxar-vue-adapter.js
@@ -13,23 +13,18 @@
 
 import Vue from 'vue';
 
-const noOp = () => {};
-
 export const technology = 'vue';
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-export function bootstrap( { widgets }, { widgetLoader } ) {
+export function bootstrap( {}, { widgetLoader, artifactProvider } ) {
 
    const { adapterErrors } = widgetLoader;
-   const widgetModules = {};
-   const activitySet = {};
-   widgets.forEach( ({ descriptor, module }) => {
-      widgetModules[ descriptor.name ] = module;
-      if( descriptor.integration.type === 'activity' ) {
-         activitySet[ descriptor.name ] = true;
-      }
-   } );
+
+   const components = {
+      widgets: {},
+      controls: {}
+   };
 
    return {
       create,
@@ -40,61 +35,41 @@ export function bootstrap( { widgets }, { widgetLoader } ) {
 
    function create( { widgetName, anchorElement, services, onBeforeControllerCreation } ) {
 
-      let vm;
+      const provider = artifactProvider.forWidget( widgetName );
+      let vm = null;
 
-      if( !widgetModules[ widgetName ] ) {
-         throw adapterErrors.unknownWidget( { technology, widgetName } );
-      }
+      return provideComponent( provider, components.widgets )
+         .then( mixinInjections )
+         .then( Component => {
+            const injections = Component.options.injections
+               .map( name => ( { [ name ]: services[ name ] } ) )
+               .reduce( ( a, b ) => ( { ...a, ...b } ) );
 
-      return {
-         domAttachTo,
-         domDetach,
-         destroy
-      };
+            onBeforeControllerCreation( injections );
 
-      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            vm = new Component( { injections } );
 
-      function createVueModel( template ) {
-         const options = widgetModules[ widgetName ];
-
-         const injectionsByName = {};
-         const injections = ( module.injections || [] ).map( injection => {
-            const value = services[ injection ];
-            if( value === undefined ) {
-               throw adapterErrors.unknownInjection( { technology, injection, widgetName } );
-            }
-            injectionsByName[ injection ] = value;
-            return value;
+            return {
+               domAttachTo,
+               domDetach,
+               destroy
+            };
          } );
-
-         onBeforeControllerCreation( injectionsByName );
-
-         return new Vue( {
-            template,
-            ...options,
-            data() {
-               const context = services.axContext;
-               const data = typeof options.data === 'function' ? options.data( injections ) : options.data;
-               return {
-                  ...context,
-                  ...data
-               }
-            }
-         } );
-      }
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       function domAttachTo( areaElement, templateHtml ) {
-         vm = createVueModel( templateHtml );
-         vm.$mount();
-         areaElement.appendChild( vm.$el );
+         if( templateHtml ) {
+            compileTemplate( services, templateHtml, vm );
+         }
+         vm.$mount( anchorElement, true );
+         areaElement.appendChild( anchorElement );
       }
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       function domDetach() {
-         const parent = vm.$el.parentNode;
+         const parent = anchorElement.parentNode;
          if( parent ) {
             parent.removeChild( anchorElement );
          }
@@ -105,8 +80,69 @@ export function bootstrap( { widgets }, { widgetLoader } ) {
       function destroy() {
          vm.$destroy();
       }
+   }
 
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   function provideComponent( artifactProviderForComponent, cache = {} ) {
+      return artifactProviderForComponent.descriptor()
+         .then( ( { name, controls = [] } ) => {
+            if( cache[ name ] ) {
+               return cache[ name ];
+            }
+
+            return ( cache[ name ] = Promise.all( [
+               artifactProviderForComponent.module(),
+               provideComponents( controls.map( artifactProvider.forControl ), components.controls )
+            ] ).then( ( [ module, controls ] ) => Vue.extend( {
+               ...module,
+               _Ctor: null,
+               components: {
+                  ...controls,
+                  ...module.components
+               }
+            } ) ) );
+         } );
+   }
+
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   function provideComponents( artifactProvidersForComponents, cache = {} ) {
+      return Promise.all( artifactProvidersForComponents.map( artifactProviderForComponent => {
+         return Promise.all( [
+            artifactProviderForComponent.descriptor(),
+            provideComponent( artifactProviderForComponent, cache )
+         ] ).then( ( [ { name }, component ] ) => ( { [ name ]: component } ) );
+      } ) ).then( controls => controls.reduce( ( a, b ) => ( { ...a, ...b } ), {} ) );
    }
 
 }
 
+function mixinInjections( Component ) {
+   const injections = Component.options.injections || [];
+
+   return Component.extend( {
+      created() {
+         this.$injections = injections
+            .map( name => this.$options.injections[ name ] );
+      },
+      data() {
+         return this.$options.injections.axContext;
+      },
+      injections: [ 'axContext' ].concat( injections )
+   } );
+}
+
+function compileTemplate( services, template, vm ) {
+   if( /\bdata-v-/.test( template ) ) {
+      services.axLog.warn( 'Widget template seems to contain data-v- prefix not supported by Vue.js.' );
+   }
+   if( !Vue.compile ) {
+      services.axLog.error( 'Compiling templates on-the-fly requires "vue" to resolve to a standalone build of Vue.js.' );
+      return;
+   }
+
+   const res = Vue.compile( template );
+   vm.$options.render = res.render;
+   vm.$options.staticRenderFns = res.staticRenderFns;
+}
